2b81256b466c34149f3560c100fcf99da80a27a3
breeze-yang <breeze@deshpro.com>
2018-05-07 15:55:55 +0800

    增加 .env控制

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'turbolinks', '~> 5'
 gem 'activeadmin', '~> 1.3'
 gem 'devise'
 
+gem 'dotenv-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# load .env
+Dotenv::Railtie.load
+
 module Kkcms
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
