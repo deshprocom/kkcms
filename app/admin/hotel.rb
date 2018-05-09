09222ea1d9c54327fb0db8b97cb22c773a5f9fa3
breeze-yang <breeze@deshpro.com>
2018-05-09 11:51:05 +0800

    增加hotel发布功能

--- app/admin/hotel.rb
+++ app/admin/hotel.rb
@@ -3,15 +3,35 @@ ActiveAdmin.register Hotel do
   filter :title
   filter :location
 
-
   index do
     render 'index', context: self
   end
 
-  permit_params :title, :logo, :location, :description
+  permit_params :title, :logo, :location, :telephone, :description
   form partial: 'form'
 
   show do
     render 'show', context: self
+  end
+
+  member_action :publish, method: :post do
+    Hotel.find(params[:id]).publish!
+    redirect_back fallback_location: admin_hotels_url, notice: I18n.t('publish_notice')
+  end
+
+  member_action :unpublish, method: :post do
+    Hotel.find(params[:id]).unpublish!
+    redirect_back fallback_location: admin_hotels_url, notice: I18n.t('unpublish_notice')
+  end
+
+  controller do
+    before_action :unpublished?, only: [:destroy]
+
+    def unpublished?
+      return unless resource.published?
+
+      flash[:error] = I18n.t('hotel.destroy_error')
+      redirect_back fallback_location: admin_hotels_url
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def img_link_to_show(resource)
     link_to resource.logo.url ? image_tag(resource.preview_logo, height: 150) : '', resource_path(resource)
   end
+
+  def hotel_publish_link(hotel)
+    if hotel.published?
+      link_to I18n.t('unpublish'), unpublish_admin_hotel_path(hotel), method: :post
+    else
+      link_to I18n.t('publish'), publish_admin_hotel_path(hotel), method: :post
+    end
+  end
 end
