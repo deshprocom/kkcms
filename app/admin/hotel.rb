ad7a2b09d6b0c627a77dad97ba7230357e3c190c
breeze-yang <breeze@deshpro.com>
2018-05-09 10:32:32 +0800

    完成hotel的 show， index method

--- app/admin/hotel.rb
+++ app/admin/hotel.rb
@@ -1,6 +1,17 @@
 ActiveAdmin.register Hotel do
-  permit_params :title, :logo, :location, :description
+  filter :id
+  filter :title
+  filter :location
 
+
+  index do
+    render 'index', context: self
+  end
+
+  permit_params :title, :logo, :location, :description
   form partial: 'form'
 
+  show do
+    render 'show', context: self
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def img_link_to_show(resource)
+    link_to resource.logo.url ? image_tag(resource.preview_logo, height: 150) : '', resource_path(resource)
+  end
 end
