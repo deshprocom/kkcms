842787df49dad918a2b89893d3c8855d88c52a78
breeze-yang <breeze@deshpro.com>
2018-06-02 18:26:10 +0800

    完成商品与运费模板的关联

--- app/shop/shipping.rb
+++ app/shop/shipping.rb
@@ -13,7 +13,17 @@ module Shop
     show do
       render 'show', context: self
     end
-    
+
+    controller do
+      before_action :shipping_used?, only: [:destroy]
+
+      def shipping_used?
+        return if resource.products.blank?
+
+        flash[:error] = '已有商品使用该运费模板，不允许删除'
+        redirect_back fallback_location: shop_expresses_url
+      end
+    end
   end
 end
 
