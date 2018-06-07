7f38e0ae1796f1e8291498aec46657890e27d77d
Ricky <rickycxj@gmail.com>
2018-06-07 15:02:15 +0800

    积分明细

--- app/admin/integral_rules.rb
+++ app/admin/integral_rules.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register IntegralRule do
+  menu priority: 5, parent: '积分管理', label: '积分规则'
   permit_params :option_type_alias, :option_type, :limit_times, :points, :opened
   config.filters = false
   config.batch_actions = false
