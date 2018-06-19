831a1763b7f2ae98cb1626f95d03153b96d6e12d
breeze-yang <breeze@deshpro.com>
2018-06-19 18:00:17 +0800

    完成酒店订单详情

--- app/admin/hotel_order.rb
+++ app/admin/hotel_order.rb
@@ -1,7 +1,12 @@
 ActiveAdmin.register HotelOrder do
   menu parent: '酒店管理'
+  actions :all, except: [:new, :edit, :create, :destroy]
 
   index do
     render 'index', context: self
+  end
+
+  show do
+    render 'show', context: self
   end
 end
