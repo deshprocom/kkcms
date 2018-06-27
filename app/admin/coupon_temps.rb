50867468ff2a74112c1affa6d4c715b5b3331e7f
Ricky <rickycxj@gmail.com>
2018-06-27 16:11:00 +0800

    优惠券后台

---
+++ app/admin/coupon_temps.rb
@@ -0,0 +1,22 @@
+ActiveAdmin.register CouponTemp do
+  menu priority: 3, parent: '优惠券', label: '模版'
+  permit_params :coupon_type, :name, :short_desc, :description, :published, :discount_type, :new_user, :limit_price, :reduce_price, :discount, :integral_on, :integrals
+  actions :all, except: [:show]
+
+  filter :new_user
+  filter :integral_on
+
+  index do
+    render 'index', context: self
+  end
+
+  form partial: 'form'
+
+  member_action :generate_coupons, method: [:get, :post] do
+    return render :coupon unless request.post?
+    params[:number].to_i.times.each do
+      Coupon.create(coupon_temp: resource, expire_day: params[:expire_day])
+    end
+    redirect_back fallback_location: admin_coupon_temps_url, notice: '创建成功'
+  end
+end
--- app/helpers/admin_trans.rb
+++ app/helpers/admin_trans.rb
@@ -6,4 +6,8 @@ module AdminTrans
   SOURCE_TYPES = %w[hotel info].collect { |d| [I18n.t("banner.#{d}"), d] }
 
   TRANS_TOPIC_STATUSES = Topic.statuses.keys.collect { |d| [I18n.t("status.#{d}"), d] }
+
+  COUPON_TYPE = CouponTemp.coupon_types.keys.collect { |d| [I18n.t("coupon_type.#{d}"), d] }
+
+  DISCOUNT_TYPE = CouponTemp.discount_types.keys.collect { |d| [I18n.t("discount_type.#{d}"), d] }
 end
